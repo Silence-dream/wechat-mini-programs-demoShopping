484fee4a45556218c20b6e1e9479358f4e737ac6
Silence-dream <1428482231@qq.com>
2020-11-27 18:05:05 +0800

    :new: 商品分类效果完成

--- miniprogram/pages/category/index.ts
+++ miniprogram/pages/category/index.ts
@@ -7,18 +7,18 @@ Page({
     leftMenuList: [],
     // 右侧商品数据
     rightConent: [],
+    currentIndex: 0,
   },
+  /* 缓存值 */
   Cates: [],
   onLoad() {
     this.getCates();
   },
-
   // 获取分页数据
   getCates() {
     request({
       url: "https://api-hmugo-web.itheima.net/api/public/v1/categories",
     }).then((result: ResponseProps) => {
-      console.log(result.data);
       this.Cates = result.data.message;
       const leftMenuList = this.Cates.map((item: CatesProps) => {
         return item.cat_name;
@@ -26,15 +26,31 @@ Page({
       const rightConent = this.Cates.map((item: CatesProps) => {
         return item.children;
       });
-      console.log(rightConent);
       /* 类型断言 */
       const leftMenuListD = leftMenuList as never[];
-      const rightConentD = leftMenuList as never[];
+      const rightConentD = rightConent as never[];
       // 保存左侧菜单数据和右侧菜单数据
       this.setData({
         leftMenuList: leftMenuListD,
-        rightConent: rightConentD,
+        rightConent: rightConentD[0],
       });
     });
+  },
+  // 点击切换商品分类
+  handCurrent(e: any) {
+    const { index } = e.currentTarget.dataset;
+    //#region 右侧分类切换
+    const rightConent = this.Cates.map((item: CatesProps) => {
+      return item.children;
+    });
+    /* 类型断言 */
+    const rightConentD = rightConent as never[];
+    // 保存左侧菜单数据和右侧菜单数据
+    this.setData({
+      rightConent: rightConentD[index],
+      // 左侧菜单选中样式切换
+      currentIndex: index,
+    });
+    //#endregion
   },
 });
