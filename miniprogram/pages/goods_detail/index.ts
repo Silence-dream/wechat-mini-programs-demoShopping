274c72ff920b5e8c5db3e51d07df0e50eb15c004
Silence-dream <1428482231@qq.com>
2020-11-28 17:39:08 +0800

    :zap: 性能优化 和 iphone适配图片

--- miniprogram/pages/goods_detail/index.ts
+++ miniprogram/pages/goods_detail/index.ts
@@ -19,7 +19,18 @@ Page({
   async getGoodsDetail<T>(goods_id: T) {
     const result = await request({ url: "/goods/detail", data: { goods_id } });
     this.setData({
-      goodsObj: result.data.message,
+      goodsObj: {
+        goods_name: result.data.message.goods_name,
+        goods_price: result.data.message.goods_price,
+        // iphone部分手机 不识别 webp图片格式
+        // 最好找到后台 让他进行修改
+        // 临时自己改 确保后台存在 1.webp => 1.jpg
+        goods_introduce: result.data.message.goods_introduce.replace(
+          /\.webp/g,
+          ".jpg"
+        ),
+        pics: result.data.message.pics,
+      },
     });
   },
 });
