cfb81ee6b7fd7273bd0a9216295f64a11640c17a
Silence-dream <1428482231@qq.com>
2021-01-11 16:10:45 +0800

    update

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,5 +1,7 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
 const index_js_1 = require("../../request/index.js");
 Page({
     data: {
